ced0c637455d6ecb38ce4cd189f22a4fa0d32b24
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-06 11:37:54 +0600

    remove comment in string->task5.js

--- String/task5.js
+++ String/task5.js
@@ -1,21 +1,12 @@
-// // Capitalize Every first Letter of each word in a String
-// let str="JavaScript is fun language";
-// console.log(str);
-// // Split the string into an array of words
-// let words = str.split(" ");
+// Capitalize Every first Letter of each word in a String
+let str="JavaScript is fun language";
+console.log(str);
+// Split the string into an array of words
+let words = str.split(" ");
 
-// for(let i=0; i<words.length; i++)
-// {
-//     words[i]=words[i][0].toUpperCase() + words[i].slice(1);
-// }
-// words=words.join(' ');
-// console.log(words);
-let str = "Mississippi";
-console.log(str.indexOf("i", 3));
-const car ={
-    name:'Toyata'
+for(let i=0; i<words.length; i++)
+{
+    words[i]=words[i][0].toUpperCase() + words[i].slice(1);
 }
-console.log(car.color);
-const sdf= "Hello";
-sdf[0]='h';
-console.log(sdf);
+words=words.join(' ');
+console.log(words);
